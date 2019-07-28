ed96ae977934923b21b96c31cfd934664287a0ed
justdj <977878634@qq.com>
2019-07-28 14:52:41 +0800

    修改上传镜像到阿里云镜像仓库

--- bin/build.sh
+++ bin/build.sh
@@ -3,8 +3,19 @@
 echo "开始构建docker镜像"
 mvn package docker:build -Dmaven.test.skip=true
 #上传到docker服务器
-sudo docker login --username=13588224627 registry.cn-qingdao.aliyuncs.com
+echo "请输入阿里云docker仓库密码"
+docker login --username=13588224627 registry.cn-hangzhou.aliyuncs.com
+#请选择上传仓库
+echo "以下是镜像仓库列表"
+echo "1.测试环境"
+echo "2.正式环境"
+echo "请输入对应数字,选择上传的镜像仓库:\c"
+read input
 imageslist=`docker images`
 var1=`echo $imageslist|awk -F ' ' '{print $9}'`
-sudo docker tag $var1 registry.cn-qingdao.aliyuncs.com/justdj/umbrella:latest
-sudo docker push registry.cn-qingdao.aliyuncs.com/justdj/umbrella:latest
+case $input in
+1)sudo docker tag [ImageId] registry.cn-hangzhou.aliyuncs.com/justdj/ugc_backend_test:latest
+  sudo docker push registry.cn-hangzhou.aliyuncs.com/justdj/ugc_backend_test:latest;;
+2)sudo docker tag [ImageId] registry.cn-hangzhou.aliyuncs.com/justdj/ugc_backend_online:latest
+  sudo docker push registry.cn-hangzhou.aliyuncs.com/justdj/ugc_backend_online:latest;;
+esacs
