a5736f28a35b89276e93d50ddf26f33dae4af5ca
justdj <977878634@qq.com>
2019-07-29 08:26:54 +0800

    添加一个测试接口

--- bin/updateOnlineService.sh
+++ bin/updateOnlineService.sh
@@ -1,4 +1,6 @@
+docker rmi $(docker images -q)
+
 docker service update \
---image registry.cn-shanghai.aliyuncs.com/justdj/ugc_backend:stable \
+--image registry.cn-shanghai.aliyuncs.com/justdj/ugc_backend_online:1.0.0 \
 --update-parallelism 1 \
 --update-delay 10s  ugc_online
