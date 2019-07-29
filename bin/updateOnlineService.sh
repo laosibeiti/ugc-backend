5fd4b75b888eb64c0a46ce5124c093780362dbdb
justdj <977878634@qq.com>
2019-07-29 21:30:33 +0800

    修改更新版本

--- bin/updateOnlineService.sh
+++ bin/updateOnlineService.sh
@@ -1,6 +1,6 @@
 docker rmi $(docker images -q)
 
 docker service update \
---image registry.cn-shanghai.aliyuncs.com/justdj/ugc_backend_online:1.0.0 \
+--image registry.cn-shanghai.aliyuncs.com/justdj/ugc_backend_online:1.0.1 \
 --update-parallelism 1 \
 --update-delay 10s  ugc_online
