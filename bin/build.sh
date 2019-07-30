62e1a7e62a7aa4a055f221b7025ce180424bfb0e
justdj <977878634@qq.com>
2019-07-30 14:31:51 +0800

    发布测试版本

--- bin/build.sh
+++ bin/build.sh
@@ -16,11 +16,6 @@ var1=`echo $imageslist|awk -F ' ' '{print $9}'`
 case $input in
 1)sudo docker tag $var1  registry-vpc.cn-shanghai.aliyuncs.com/justdj/ugc_backend_test:1.0.0
   sudo docker push  registry-vpc.cn-shanghai.aliyuncs.com/justdj/ugc_backend_test:1.0.0
-    echo "test 服务滚动升级"
-  docker service update \
-  --image registry.cn-shanghai.aliyuncs.com/justdj/ugc_backend_test:1.0.0 \
-  --update-parallelism 1 \
-  --update-delay 10s  ugc_online;;
 2)sudo docker tag $var1  registry-vpc.cn-shanghai.aliyuncs.com/justdj/ugc_backend_online:1.0.5
   sudo docker push  registry-vpc.cn-shanghai.aliyuncs.com/justdj/ugc_backend_online:1.0.5
   echo "online 服务滚动升级"
