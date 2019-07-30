71de22df3f9b93df36c4060f56495e1aa08c9e5b
justdj <977878634@qq.com>
2019-07-30 21:35:48 +0800

    后端架构基本完成

--- bin/startOnlineService.sh
+++ bin/startOnlineService.sh
@@ -1,10 +1,19 @@
 docker network create -d overlay ugc_online
 
+--------------
+
 docker rmi $(docker images -q) -f
 
 docker service create --name ugc_online \
 --network ugc_online \
 --endpoint-mode vip \
--p 9002:9002 \
---replicas 3 \
+-p 9009:9002 \
+--replicas 2 \
 registry.cn-shanghai.aliyuncs.com/justdj/ugc_backend_online:1.1.0
+
+docker service create --name ugc_test \
+--network ugc_test \
+--endpoint-mode vip \
+-p 9002:9002 \
+--replicas 2 \
+registry.cn-shanghai.aliyuncs.com/justdj/ugc_backend_test:1.0.0
