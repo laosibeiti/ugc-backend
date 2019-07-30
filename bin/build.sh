7f034cc21cd1136a1bdd75b6eeb05c966e71b658
justdj <977878634@qq.com>
2019-07-30 15:49:35 +0800

    更新线上服务 尝试修复profile不生效问题

--- bin/build.sh
+++ bin/build.sh
@@ -16,11 +16,6 @@ var1=`echo $imageslist|awk -F ' ' '{print $9}'`
 case $input in
 1)sudo docker tag $var1  registry-vpc.cn-shanghai.aliyuncs.com/justdj/ugc_backend_test:1.0.0
   sudo docker push  registry-vpc.cn-shanghai.aliyuncs.com/justdj/ugc_backend_test:1.0.0;;
-2)sudo docker tag $var1  registry-vpc.cn-shanghai.aliyuncs.com/justdj/ugc_backend_online:1.0.7
-  sudo docker push  registry-vpc.cn-shanghai.aliyuncs.com/justdj/ugc_backend_online:1.0.7
-  echo "online 服务滚动升级"
-   docker service update \
-   --image registry.cn-shanghai.aliyuncs.com/justdj/ugc_backend_online:1.0.7 \
-   --update-parallelism 1 \
-   --update-delay 10s  ugc_online;;
+2)sudo docker tag $var1  registry-vpc.cn-shanghai.aliyuncs.com/justdj/ugc_backend_online:1.0.8
+  sudo docker push  registry-vpc.cn-shanghai.aliyuncs.com/justdj/ugc_backend_online:1.0.8;;
 esac
